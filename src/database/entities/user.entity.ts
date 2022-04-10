a2ce827d079e60ccc72e19dfda2cdb3506a57bb0
Yuriy <yfrolou@mail.ru>
2022-04-10 21:51:12 +0300

    added role entity

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -6,7 +6,7 @@ import {
 } from 'typeorm';
 import {PostEntity} from "./post.entity";
 import {CommentEntity} from "./comment.entity";
-import {IsEmail, IsNotEmpty, IsString} from "class-validator";
+import {IsEmail, IsEnum, IsNotEmpty, IsString} from "class-validator";
 
 @Entity('users')
 export class UserEntity {
@@ -24,6 +24,13 @@ export class UserEntity {
 
     @Column({ name: 'role', type: 'text' })
     role!: string;
+
+    // @Column({ name: 'role', type: 'text' })
+    // @IsEnum(Role) roles: Role;
+
+
+    @Column({ name: 'password', type: 'text' })
+    password: string;
 
     @OneToMany(() => PostEntity, (post) => post.user)
     posts: PostEntity[];
